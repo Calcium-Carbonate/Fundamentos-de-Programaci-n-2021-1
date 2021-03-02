f7230be3b807c2ea089ca85f3dd9d55bc2a0745e
Calcium-Carbonate <77901431+Calcium-Carbonate@users.noreply.github.com>
2021-03-01 20:31:41 -0500

    Add files via upload

---
+++ 09 Ciclos (simulacro p2) T2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _09_Ciclos__simulacro_p2__T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido al otro jueguito de dados");
+
+            Random azar = new Random();
+            int dado1 = 0,dado2=0, turnos = 0, turnoVisual=0, total = 0, vidas = 3;
+            Console.WriteLine("Desea empezar(S/N)");
+
+            string continuar = Console.ReadLine();
+            while (continuar != "S" && continuar != "N")
+            {
+                Console.WriteLine("Por favor ingrese una respuesta Si(S) o No(N)");
+
+                continuar = Console.ReadLine();
+            }
+
+
+            while (continuar == "S")
+            {
+                turnos++; //turnos para el contador para usar los dos dados
+                turnoVisual++; //turnos totales para usar el modulo
+                if (turnoVisual % 2 == 0)//restar vidas los turnos pares
+                {
+                    vidas--;
+                }
+
+                dado1 = azar.Next(1, 7);
+                
+                dado2 = 0;
+                
+                total += dado1;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Este fue su dado este turno " + dado1);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Su puntaje va en " + total);
+                Console.WriteLine("Usted tiene " + vidas + " vidas");
+                Console.WriteLine("Número de turnos hasta ahora " + turnoVisual);
+
+
+               
+                if (turnos ==3)//Cada 3 turnos 
+                {
+                    turnos = 0;
+                    //Trampita sensual
+                    //dado2=int.Parse(Console.ReadLine());
+                    dado2 = azar.Next(1, 7);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Este fue su segundo dado este turno " + dado2);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    total += dado2;
+                    Console.WriteLine("Su puntaje va en " + total);
+                }
+                if (dado1 == dado2)
+                {
+                    vidas++;
+                    Console.WriteLine("Has obtenido una vida!");
+                    Console.WriteLine("Vidas " + vidas);
+
+                }
+                if (vidas == 0)//Se acaban las vidas
+                {
+                    Console.WriteLine("Te quedaste sin vidas, has pedido");
+                    break;
+                }
+
+                if (total >= 100)//victoria
+                {
+                    Console.WriteLine("Felicidades usted obtuvo un total de  " + total + " y alcanzó la victoria");
+                    break;
+                }
+
+                Console.WriteLine("Desea continuar? (S/N)");
+                continuar = Console.ReadLine();
+                while (continuar != "S" && continuar != "N")
+                {
+                    Console.WriteLine("Por favor ingrese una respuesta Si(S) o No(N)");
+
+                    continuar = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Su total fue de  " + total);
+            Console.WriteLine("Fin del juego");
+
+        }
+    }
+    //video solución https://youtu.be/7ghgf180OzY
+}
+
---
+++ Ciclos (simulacro p2)T1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Ciclos__simulacro_p2_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al jueguito de dados");
+
+            Random azar = new Random();
+            int dado1 = 0, dadoEsp = 0, contadorEps = 2, contadorU = 0, contadorS = 0, total = 0, vidas = 3, turnos = 0;
+
+            string especial = "";
+
+            Console.WriteLine("Desea empezar(S/N)");
+
+            string continuar = Console.ReadLine();
+
+            while (continuar != "S" && continuar != "N") //validar
+            {
+                Console.WriteLine("Por favor ingrese una respuesta Si(S) o No(N)");
+
+                continuar = Console.ReadLine();
+            }
+
+            while (continuar == "S")
+            {
+                turnos++;
+
+                Console.WriteLine("Su puntaje va en " + total);
+
+                Console.WriteLine("Usted tiene " + vidas + " vidas");
+
+                Console.WriteLine("Estos son sus dados especiales " + contadorEps);
+
+                dadoEsp = 0;
+
+                if (contadorEps > 0) //Si el contador de dados especiales es mayor a 0 se le pregunta al usuario si quiere utilizar uno
+                {
+                    Console.WriteLine("Desea utilizar sus dados especiales (S/N)");
+
+                    especial = Console.ReadLine();
+                    while (especial != "S" && especial != "N") //Validar
+                    {
+                        Console.WriteLine("Por favor ingrese una respuesta Si(S) o No(N)");
+
+                        especial = Console.ReadLine();  
+                    }
+                }
+
+                if (especial == "S")
+                {
+                    dadoEsp = azar.Next(1, 13);
+                    contadorEps--;
+                    dado1 = dadoEsp;
+                    especial = "";
+                }
+ 
+                else { dado1 = azar.Next(1, 7); }
+
+                //trampita sensual
+                //dado1 = int.Parse(Console.ReadLine());
+
+                total += dado1;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Este fue su dado este turno " + dado1);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                if (total >= 100)
+                {
+                    Console.WriteLine("Felicidades usted obtuvo un total de  " + total + " y alcanzó la victoria");
+                    break;
+                }
+
+                if (dado1 == 1)
+                {
+                    contadorU++;
+                    if (contadorU == 1) { Console.WriteLine("Si sacas otro 1 perderás una vida"); }
+
+
+                    if (contadorU == 2)
+                    {
+                        Console.WriteLine("Sacaste dos veces 1, se te restará una vida");
+                        vidas--;
+                        total -= 10;
+                        contadorU = 0;
+                        Console.WriteLine("VIDAS  " + vidas);
+                        Console.WriteLine("Su total este turno  " + total);
+                    }
+                }
+
+                if (vidas == 0)
+                {
+                    Console.WriteLine("Te quedaste sin vidas, has pedido");
+                    break;
+                }
+
+                if (dado1 == 6)
+                {
+                    contadorS++;
+
+                    if (contadorS == 1) { Console.WriteLine("Si sacas otro seis sumaras otra vida "); }
+
+
+                    if (contadorS == 2 && vidas < 3)
+                    {
+                        vidas++;
+                        Console.WriteLine("Obtuviste una vida al sacar 6 dos veces seguidas");
+                        Console.WriteLine("VIDAS  " + vidas);
+                    }
+                }
+                else
+                {
+                    contadorS = 0;
+                }
+
+                Console.WriteLine("Desea continuar? (S/N)");
+                continuar = Console.ReadLine();
+                while (continuar != "S" && continuar != "N")
+                {
+                    Console.WriteLine("Por favor ingrese una respuesta Si(S) o No(N)");
+
+                    continuar = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Su total fue de  " + total);
+            Console.WriteLine("Sus vidas   " + vidas);
+            Console.WriteLine("Número de turnos " + turnos);
+            Console.WriteLine("Fin del juego");
+        }
+    }
+
+    //VIDEO SOLUCIÓN https://youtu.be/7ghgf180OzY
+}
+
